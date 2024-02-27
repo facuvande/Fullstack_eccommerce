50c65c30dcd6d310404e50c018f22ded145755b6
Facundo <facundovs84@hotmail.com.ar>
2024-02-27 16:42:35 -0300

    Implementacion metodo doFilterInternal

--- Server/users-service/src/main/java/com/usersservice/security/JwtAuthenticationFilter.java
+++ Server/users-service/src/main/java/com/usersservice/security/JwtAuthenticationFilter.java
@@ -5,15 +5,26 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private CustomUsersDetailsService customUsersDetailsService;
+    @Autowired
+    private JwtTokenGenerator jwtGenerator;
+
 
     private String getTokenByRequest(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
@@ -26,6 +37,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getTokenByRequest(request);
-        if(StringUtils.hasText(token) && )
+        if(StringUtils.hasText(token) && jwtGenerator.validateToken(token)){
+            String username = jwtGenerator.getUsernameByJwt(token);
+            UserDetails userDetails = customUsersDetailsService.loadUserByUsername(username);
+            List<String> userRoles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+
+            if(userRoles.contains("USER") || userRoles.contains("ADMIN")){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
