b44e74849fe433e8721ec2c8e1a3c03dd0d5735b
Facundo <facundovs84@hotmail.com.ar>
2024-02-27 16:25:59 -0300

    Creacion de CustomUsersDetailsService

---
+++ Server/users-service/src/main/java/com/usersservice/security/CustomUsersDetailsService.java
@@ -0,0 +1,32 @@
+package com.usersservice.security;
+
+import com.usersservice.model.Role;
+import com.usersservice.model.User;
+import com.usersservice.repository.IUserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUsersDetailsService implements UserDetailsService {
+    private IUserRepository userRepository;
+
+    // Metodo para traer lista de autoridades mediante lista de roles
+    public Collection<GrantedAuthority> mapToAuthorities(List<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    // Traer usuario con todos sus datos mediante username(email)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), mapToAuthorities(user.getRol()));
+    }
+}
