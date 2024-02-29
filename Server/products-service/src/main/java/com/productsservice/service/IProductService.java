38fd94a470b80d1426174036cfde89c446de4a4c
Facundo <facundovs84@hotmail.com.ar>
2024-02-29 12:12:53 -0300

    Creacion de metodos de product

--- Server/products-service/src/main/java/com/productsservice/service/IProductService.java
+++ Server/products-service/src/main/java/com/productsservice/service/IProductService.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface IProductService {
 
+    public Product createProduct(Product product);
+    public List<Product> getProducts();
+    public Product getProductById(Long id_product);
+    public Product editProduct(Product product);
+    public void deleteProductById(Long id_product);
+
 }
