75ad0c2218347b5eb17f8642acb011bfbd6a5af5
Facundo <facundovs84@hotmail.com.ar>
2024-03-07 15:43:30 -0300

    Carga de productos desde frontend y solucion de error de CORS

--- Client/tienda_newstack/src/ecommerce/components/Products.jsx
+++ Client/tienda_newstack/src/ecommerce/components/Products.jsx
@@ -2,84 +2,39 @@ import React from 'react'
 import './Products.css'
 import { GoHeartFill } from "react-icons/go";
 import { Link } from 'react-router-dom';
-
-const products = [
-    {
-        id: 1,
-        name: 'Refrigerador Sphing 2000',
-        marca: 'Sphing',
-        description: 'Descripcion del producto 1',
-        price: 500,
-        stock: 10,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 2,
-        name: 'Auricular 2.0 Fony funy',
-        marca: 'Fony',
-        description: 'Descripcion del producto 2',
-        price: 1000,
-        stock: 5,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 3,
-        name: 'Notebook 3.0',
-        marca: 'Chinwenwencha',
-        description: 'Descripcion del producto 3',
-        price: 1500,
-        stock: 3,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 4,
-        name: 'Notebook 3.0',
-        marca: 'Chinwenwencha',
-        description: 'Descripcion del producto 3',
-        price: 1500,
-        stock: 3,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 4,
-        name: 'Notebook 3.0',
-        marca: 'Chinwenwencha',
-        description: 'Descripcion del producto 3',
-        price: 1500,
-        stock: 3,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 4,
-        name: 'Notebook 3.0',
-        marca: 'Chinwenwencha',
-        description: 'Descripcion del producto 3',
-        price: 1500,
-        stock: 3,
-        img: 'https://via.placeholder.com/150'
-    },
-    {
-        id: 4,
-        name: 'Notebook 3.0',
-        marca: 'Chinwenwencha',
-        description: 'Descripcion del producto 3',
-        price: 1500,
-        stock: 3,
-        img: 'https://via.placeholder.com/150'
-    }
-]
+import { useEffect, useState } from 'react';
 
 export const Products = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8083/products', {
+            method: 'GET',
+    })
+        .then(response => {
+            if(response.ok){
+                response.json().then(data => {
+                    setProducts(data)
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [])
+    
+
     return (
         <div className='products-container'>
             <h2>Productos</h2>
             <div className='products'>
                 {products.map(product => {
                     return (
-                        <div key={product.id} className='product'>
+                        <div key={product.id_product} className='product'>
                             <img src={product.img} alt={product.name}/>
                             <h3>{product.name}</h3>
-                            <Link to={`/products/${product.id}`}>Ver detalles</Link>
+                            <Link to={`/products/${product.id_product}`}>Ver detalles</Link>
                             <div className='actions'>
                                 <button>Agregar al carrito</button>
                                 <GoHeartFill className='addFavorite'/>
--- Server/products-service/src/main/java/com/productsservice/ProductsServiceApplication.java
+++ Server/products-service/src/main/java/com/productsservice/ProductsServiceApplication.java
@@ -4,14 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class ProductsServiceApplication {
+public class ProductsServiceApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry){
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*");
 	}
 
 }
