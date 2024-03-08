9e65f987fb8c875614488f63775f3d9c88e25c77
Facundo <facundovs84@hotmail.com.ar>
2024-03-08 14:12:58 -0300

    Incorporacion de axios

--- Client/tienda_newstack/src/pages/RegisterPage.jsx
+++ Client/tienda_newstack/src/pages/RegisterPage.jsx
@@ -7,41 +7,44 @@ import password_icon from '../assets/password.png'
 import './RegisterPage.css'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 export const RegisterPage = () => {
 
-    const [name, setName] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const { register, handleSubmit } = useForm();
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
+    // const [name, setName] = useState('')
+    // const [lastname, setLastname] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
 
-        try {
-            const response = await fetch('http://localhost:8082/users/auth/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name,
-                    lastname,
-                    email,
-                    password,
-                })
-            })
+    // const handleSubmit = async(e) => {
+    //     e.preventDefault();
 
-            const data = await response.json();
-            console.log(data.message);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //     try {
+    //         const response = await fetch('http://localhost:8082/users/auth/register', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({
+    //                 name,
+    //                 lastname,
+    //                 email,
+    //                 password,
+    //             })
+    //         })
+
+    //         const data = await response.json();
+    //         console.log(data.message);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     return (
         <div className='container'>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit( values => console.log(values))}>
                 <div className='header'>
                     <div className="text">Crear una cuenta</div>
                     <div className='underline'></div>
@@ -49,19 +52,19 @@ export const RegisterPage = () => {
                 <div className='inputs'>
                     <div className='input'>
                         <img src={user_icon} alt=''/>
-                        <input type='text' placeholder='Nombre' value={name} onChange={(e) => setName(e.target.value)}/>
+                        <input type='text' placeholder='Nombre' {...register("name", { required: true })}/>
                     </div>
                     <div className='input'>
                         <img src={user_icon} alt=''/>
-                        <input type='text' placeholder='Apellido' value={lastname} onChange={(e) => setLastname(e.target.value)}/>
+                        <input type='text' placeholder='Apellido' {...register("lastname", { required: true })}/>
                     </div>
                     <div className='input'>
                         <img src={email_icon} alt=''/>
-                        <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <input type='email' placeholder='Email' {...register("email", { required: true })}/>
                     </div>
                     <div className='input'>
                         <img src={password_icon} alt=''/>
-                        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <input type='password' placeholder='Password' {...register("password", { required: true })}/>
                     </div>
                 </div>
                 <div className="login-redirect">Ya tienes cuenta? <Link to="/auth/login">Logeate!</Link></div>
