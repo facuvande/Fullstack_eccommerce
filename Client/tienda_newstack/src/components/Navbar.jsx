cb12e9d88a10e33fc31e7dd2a9507fdfec248e8b
Facundo <facundovs84@hotmail.com.ar>
2024-03-14 19:50:22 -0300

    Solucion boton profile se rompia en vista mobile

--- Client/tienda_newstack/src/components/Navbar.jsx
+++ Client/tienda_newstack/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ export const Navbar = () => {
                     }
                     {
                         user 
-                            ?   <Link to='/profile' className='bg-blue-700 text-white py-2 px-6 rounded md:ml-8 hover:bg-blue-500 duration-500'><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg></Link>
+                            ?   <Link to='/profile' className='bg-blue-700 text-white py-2 px-6 rounded md:ml-8 hover:bg-blue-500 duration-500 inline-block'><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg></Link>
                             :   <Link to='/login' className='bg-blue-700 text-white py-2 px-6 rounded md:ml-8 hover:bg-blue-500 duration-500'>
                                     Iniciar sesion
                                 </Link>
--- Client/tienda_newstack/src/components/ProfileUser.jsx
+++ Client/tienda_newstack/src/components/ProfileUser.jsx
@@ -28,11 +28,11 @@ export const ProfileUser = ({user}) => {
         <form className="max-w-md mx-auto mt-40" onSubmit={onSubmit}>
             <h2 className="mb-8 text-3xl font-extrabold tracking-tight leading-none text-gray-900 sm:text-4xl dark:text-gray-900">Editar usuario</h2>
             <div className="grid md:grid-cols-2 md:gap-6">
-                <div className="relative z-0 w-full mb-5 group">
+                <div className="relative  w-full mb-5 group">
                     <input type="text" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " {...register("name", { required: true })} />
-                    <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre</label>
+                    <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre</label>
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
+                <div className="relative  w-full mb-5 group">
                     <input type="text" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " {...register("lastname", { required: true })} />
                     <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Apellido</label>
                 </div>
