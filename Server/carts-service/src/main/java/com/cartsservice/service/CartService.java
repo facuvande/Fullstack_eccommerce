696b7d0ed5d2318bb9354b064e5ecd223a1cd11b
Facundo <facundovs84@hotmail.com.ar>
2024-03-22 13:13:54 -0300

    Creacion de CartService

---
+++ Server/carts-service/src/main/java/com/cartsservice/service/CartService.java
@@ -0,0 +1,7 @@
+package com.cartsservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+}
---
+++ Server/carts-service/src/main/java/com/cartsservice/service/ICartService.java
@@ -0,0 +1,4 @@
+package com.cartsservice.service;
+
+public interface ICartService {
+}
