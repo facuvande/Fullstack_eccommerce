f2c3a92789e4fb0b4e1953cdeda91b749e4ff4d3
Facundo <facundovs84@hotmail.com.ar>
2024-03-22 12:57:39 -0300

    Creacion de cart-service

---
+++ Server/carts-service/src/test/java/com/cartsservice/CartsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
