222e2adc902fc4a88f1296e34ea8cab9e9c45076
Facundo <facundovs84@hotmail.com.ar>
2024-04-04 15:47:30 -0300

    Sacado sombreado de boton comprar

--- Client/tienda_newstack/src/components/Cart.jsx
+++ Client/tienda_newstack/src/components/Cart.jsx
@@ -81,7 +81,7 @@ export const Cart = () => {
                             ))
                         }
                     <h3 className='w-full mt-4 text-xl text-black font-semibold'>Total: ${cart.total_ammount}</h3>
-                    <button type="button" className="text-white mt-4 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 ">Comprar</button>
+                    <button type="button" className="text-white mt-4 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 ">Comprar</button>
                     </tbody>
                 </table>
                 
