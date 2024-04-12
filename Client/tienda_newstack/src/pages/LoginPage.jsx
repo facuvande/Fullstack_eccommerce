5b56431b407a2690e884e003bc77cf3aec707d4e
Facundo <facundovs84@hotmail.com.ar>
2024-04-12 13:41:59 -0300

    Agregado Payments-service

--- Client/tienda_newstack/src/pages/LoginPage.jsx
+++ Client/tienda_newstack/src/pages/LoginPage.jsx
@@ -26,21 +26,20 @@ export const LoginPage = () => {
                 <div className='w-[61px] h-[6px] bg-blue-500 rounded-[9px] m-auto mt-2'></div>
             </div>
 
-            <form class="max-w-sm mx-auto" onSubmit={onSubmit}>
-                <div class="mb-5">
-                    <label for="email" class="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Email</label>
-                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com"  {...register ("email", { required: true })} />
+            <form className="max-w-sm mx-auto" onSubmit={onSubmit}>
+                <div className="mb-5">
+                    <label htmlFor="email" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com"  {...register ("email", { required: true })} />
                     { errors.email && <span className="error">Este campo es requerido</span> }
                 </div>
-                <div class="mb-5">
-                    <label for="password" class="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Contraseña</label>
-                    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...register ("password", { required: true })} />
+                <div className="mb-5">
+                    <label htmlFor="password" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Contraseña</label>
+                    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...register ("password", { required: true })} />
                     { errors.password && <span className="error">Este campo es requerido</span> }
                 </div>
-                <div className="">Olvidaste tu contraseña? <Link to="/login" className='text-blue-300'>Click aqui!</Link></div>
                 <div className="mb-5">No tienes cuenta? <Link to="/register" className='text-blue-300'>Registrate</Link></div>
                 { LoginErrors && <div className="error mb-5" style={{textAlign: 'center'}}>{LoginErrors}</div>}
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar sesion</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar sesion</button>
             </form>
         </div>
     )
