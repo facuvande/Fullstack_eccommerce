abfb8f88eb16414a230c44260a8b64c0e6b514b3
Facundo <facundovs84@hotmail.com.ar>
2024-04-14 13:01:08 -0300

    Modificacion en estilos de RegisterPage

--- Client/tienda_newstack/src/components/FooterNav.jsx
+++ Client/tienda_newstack/src/components/FooterNav.jsx
@@ -13,7 +13,7 @@ export const FooterNav = ({changeSeccion}) => {
 
     return (
         <nav className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-slate-950 dark:border-gray-600">
-            <div className={isUserAdmin ? "grid h-full max-w-lg grid-cols-6 mx-auto font-medium" : "grid h-full max-w-lg grid-cols-4 mx-auto font-medium"}>
+            <div className={isUserAdmin ? "grid h-full max-w-lg grid-cols-5 mx-auto font-medium" : "grid h-full max-w-lg grid-cols-4 mx-auto font-medium"}>
                 <button type="button" onClick={() => changeSeccion('profile')} className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                     <svg className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
@@ -24,10 +24,12 @@ export const FooterNav = ({changeSeccion}) => {
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="text-gray-500 dark:text-gray-400"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
                     <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Favoritos</span>
                 </button>
-                <button type="button" onClick={() => changeSeccion('cart')} className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                {
+                    !isUserAdmin && <button type="button" onClick={() => changeSeccion('cart')} className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="text-gray-500 dark:text-gray-400"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
                     <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Carrito</span>
-                </button>
+                    </button>
+                }
                 {
                     isUserAdmin && <button type="button" onClick={() => changeSeccion('edit-users')}  className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className='text-gray-500 dark:text-gray-400'><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
--- Client/tienda_newstack/src/pages/RegisterPage.jsx
+++ Client/tienda_newstack/src/pages/RegisterPage.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
-import user_icon from '../assets/person.png'
-import email_icon from '../assets/email.png'
-import password_icon from '../assets/password.png'
-
-import './RegisterPage.css'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { useAuth } from '../context/AuthContext'
@@ -25,39 +20,35 @@ export const RegisterPage = () => {
     })
 
     return (
-        <div className='container'>
-            <form onSubmit={onSubmit}>
-                <div className='header'>
-                    <div className="text">Crear una cuenta</div>
-                    <div className='underline'></div>
+        <div className='bg-slate-800 h-[100vh] flex flex-col items-center justify-center text-center m-auto p-9 text-white'>
+            <div className='mb-8'>
+                <div className="font-bold text-4xl">Crear una cuenta</div>
+                <div className='w-[61px] h-[6px] bg-blue-500 rounded-[9px] m-auto mt-2'></div>
+            </div>
+            <form className="max-w-sm mx-auto" onSubmit={onSubmit}>
+                <div className="mb-5">
+                    <label htmlFor="name" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Nombre</label>
+                    <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" {...register("name", { required: true })}/>
+                    { errors.name && <span className="error">Este campo es requerido</span> }
                 </div>
-                <div className='inputs'>
-                    <div className='input'>
-                        <img src={user_icon} alt=''/>
-                        <input type='text' placeholder='Nombre' {...register("name", { required: true })}/>
-                        { errors.name && <span className="error">Este campo es requerido</span> }
-                    </div>
-                    <div className='input'>
-                        <img src={user_icon} alt=''/>
-                        <input type='text' placeholder='Apellido' {...register("lastname", { required: true })}/>
-                        { errors.lastname && <span className="error">Este campo es requerido</span> }
-                    </div>
-                    <div className='input'>
-                        <img src={email_icon} alt=''/>
-                        <input type='email' placeholder='Email' {...register("email", { required: true })}/>
-                        { errors.email && <span className="error">Este campo es requerido</span> }
-                    </div>
-                    <div className='input'>
-                        <img src={password_icon} alt=''/>
-                        <input type='password' placeholder='Password' {...register("password", { required: true })}/>
-                        { errors.password && <span className="error">Este campo es requerido</span>}
-                    </div>
+                <div className="mb-5">
+                    <label htmlFor="lastname" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Apellido</label>
+                    <input type="text" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Apellido" {...register("lastname", { required: true })}/>
+                    { errors.lastname && <span className="error">Este campo es requerido</span> }
                 </div>
-                <div className="login-redirect">Ya tienes cuenta? <Link to="/login">Logeate!</Link></div>
-                { RegisterErrors && <div className="error" style={{textAlign: 'center'}}>{RegisterErrors}</div>}
-                <div className="submit-container">
-                    <button type='submit' className="submit">Continuar</button>
+                <div className="mb-5">
+                    <label htmlFor="email" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com" {...register("email", { required: true })}/>
+                    { errors.email && <span className="error">Este campo es requerido</span> }
                 </div>
+                <div className="mb-5">
+                    <label htmlFor="password" className="block mb-2 text-2xl font-medium text-gray-900 dark:text-white">Contraseña</label>
+                    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" {...register("password", { required: true })}/>
+                    { errors.password && <span className="error">Este campo es requerido</span>}
+                </div>
+                <div className="mb-5">Ya tienes cuenta? <Link to="/login" className='text-blue-300'>Logeate!</Link></div>
+                { RegisterErrors && <div className="error mb-5" style={{textAlign: 'center'}}>{RegisterErrors}</div>}
+                <button type='submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continuar</button>
             </form>
         </div>
     )
